60a345dd378bb069afded83a71d7e67b3cb5daaf
Somyaranjan Khatua <somyaranjankhatua122@gmail.com>
2023-09-13 12:27:38 +0000

    vault contract created

--- contracts/vault/src/contract.rs
+++ contracts/vault/src/contract.rs
@@ -1,10 +1,11 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Reply, Response, StdResult};
+use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Reply, Response, StdResult, Uint128};
 use cw2::set_contract_version;
 
 use crate::error::ContractError;
-use crate::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
+use crate::msg::{ExecuteMsg, InstantiateMsg, AddLiquidityParams};
+use crate::state::{PoolData, FACTORY_REGISTER, POOL_REGISTER, VAULT_OWNER};
 
 const CONTRACT_NAME: &str = "crates.io:vault";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -18,17 +19,18 @@ pub fn instantiate(
 ) -> Result<Response, ContractError> {
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
 
+    // Save the vault owner's address in the VAULT_OWNER
+    VAULT_OWNER.save(deps.storage, &info.sender.to_string())?;
+
     Ok(Response::new()
         .add_attribute("method", "instantiate")
         .add_attribute("owner", info.sender))
 }
 
-#[cfg_attr(not(feature = "library"), entry_point)]
-pub fn migrate(_deps: DepsMut, _env: Env, msg: MigrateMsg) -> Result<Response, ContractError> {
-    match msg { 
-
-    }
-}
+// #[cfg_attr(not(feature = "library"), entry_point)]
+// pub fn migrate(_deps: DepsMut, _env: Env, msg: MigrateMsg) -> Result<Response, ContractError> {
+//     match msg {}
+// }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
@@ -38,16 +40,122 @@ pub fn execute(
     msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
     match msg {
-
+        ExecuteMsg::RegisterFactory { factory_address } => {
+            execute::execute_register_factory(_deps, _env, _info, factory_address)
+        }
+        ExecuteMsg::RegisterPool {
+            pool_address,
+            token0,
+            token1,
+            lp_token_contract,
+        } => execute::execute_register_pool(
+            _deps,
+            _env,
+            _info,
+            pool_address,
+            token0,
+            token1,
+            lp_token_contract,
+        ),
+        ExecuteMsg::AddLiquidity(add_liquidity_params) => execute::execute_add_liquidity(_deps, _env, _info, add_liquidity_params)
     }
 }
 
-#[cfg_attr(not(feature = "library"), entry_point)]
-pub fn query(_deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
-    match msg {
+pub mod execute {
+    use super::*;
 
+    pub fn execute_register_factory(
+        _deps: DepsMut,
+        _env: Env,
+        _info: MessageInfo,
+        _factory: String,
+    ) -> Result<Response, ContractError> {
+        // Load the vault owner from storage
+        let vault_owner = VAULT_OWNER.load(_deps.storage);
+
+        match vault_owner {
+            Ok(owner) => {
+                // Check if the sender is authorized to register the factory
+                if owner != _info.sender {
+                    // Return an error if the sender is not authorized
+                    return Err(ContractError::CustomError {
+                        val: "Unauthorized Caller!".to_string(),
+                    });
+                } else {
+                    // Save the factory registration status as true in storage
+                    FACTORY_REGISTER.save(_deps.storage, _factory.clone(), &true)?;
+
+                    // Return a successful response with attributes
+                    Ok(Response::new()
+                        .add_attribute("function", "value")
+                        .add_attribute("factory_contract_address", _factory.to_string()))
+                }
+            }
+            Err(_) => {
+                // Return an error if the vault owner is not found in storage
+                return Err(ContractError::CustomError {
+                    val: "Unable to find vault owner".to_string(),
+                });
+            }
+        }
+    }
+
+    pub fn execute_register_pool(
+        _deps: DepsMut,
+        _env: Env,
+        _info: MessageInfo,
+        _pool: String,
+        _token0: String,
+        _token1: String,
+        _lp_token_contract: String,
+    ) -> Result<Response, ContractError> {
+        // Check if the factory contract is registered
+        let factory_registered = FACTORY_REGISTER.load(_deps.storage, _info.sender.to_string());
+
+        match factory_registered {
+            Ok(registered) => {
+                // If factory contract is not registered, return an error
+                if registered != true {
+                    return Err(ContractError::CustomError {
+                        val: "Unauthorized factory contract!".to_string(),
+                    });
+                } else {
+                    // Create a new `PoolData` instance to store pool registration information
+                    let pool_data = PoolData {
+                        registered: true,
+                        token0: _token0,
+                        token1: _token1,
+                        reserve0: Uint128::zero(),
+                        reserve1: Uint128::zero(),
+                        lp_token_contract: _lp_token_contract,
+                    };
+
+                    // Save the pool registration data in the `POOL_REGISTER` mapping
+                    POOL_REGISTER.save(_deps.storage, _pool.clone(), &pool_data)?;
+
+                    // Return a successful response with attributes
+                    Ok(Response::new()
+                        .add_attribute("function", "execute_register_pool")
+                        .add_attribute("pool_contract_address", _pool))
+                }
+            }
+            Err(_) => {
+                return Err(ContractError::CustomError {
+                    val: "Unable to find factory contract!".to_string(),
+                })
+            }
+        }
+    }
+
+    pub fn execute_add_liquidity(_deps: DepsMut, _env: Env, _info: MessageInfo, _add_liquidity_params: AddLiquidityParams) -> Result<Response, ContractError> {
+        todo!()
     }
 }
+
+// #[cfg_attr(not(feature = "library"), entry_point)]
+// pub fn query(_deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
+//     match msg {}
+// }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn reply(_deps: DepsMut, _env: Env, _msg: Reply) -> Result<Response, ContractError> {
--- contracts/vault/src/msg.rs
+++ contracts/vault/src/msg.rs
@@ -1,4 +1,7 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
+use cosmwasm_std::Uint256;
+use schemars::JsonSchema;
+use serde::{ Serialize, Deserialize };
 
 /// Message type for `instantiate` entry_point
 #[cw_serde]
@@ -6,7 +9,68 @@ pub struct InstantiateMsg {}
 
 /// Message type for `execute` entry_point
 #[cw_serde]
-pub enum ExecuteMsg {}
+pub enum ExecuteMsg {
+    /**
+     * 1. RegisterFactory: This function allows the owner of the vault contract to register a factory contract.
+     * Only the owner of the vault contract can call this function.
+     *
+     * Parameters:
+     * - `factory_address`: The address of the factory contract to be registered.
+     */
+    RegisterFactory { factory_address: String },
+
+    /**
+     * 2. RegisterPool: This function is used to register a pool contract in the vault.
+     *
+     * Parameters:
+     * - `pool_address`: The address of the pool contract to be registered.
+     * - `token0`: The address or identifier of the first token in the token pair managed by the pool.
+     * - `token1`: The address or identifier of the second token in the token pair managed by the pool.
+     * - `lp_token_contract`: The address of the CW20 contract responsible for minting LP tokens when users provide liquidity to the pool.
+     *
+     * In a liquidity pool, `token0` and `token1` represent a token pair that the pool manages. Users can provide liquidity in the form of both `token0` and `token1`, and in return, they receive LP (Liquidity Provider) tokens from the `lp_token_contract`.
+     */
+    RegisterPool {
+        pool_address: String,
+        token0: String,
+        token1: String,
+        lp_token_contract: String,
+    },
+
+    /**
+     * AddLiquidity: This function allows a user to add liquidity to a specific pool contract.
+     *
+     * Parameters are defined in AddLiquidityParams:
+     * - `pool_address`: The address of the pool contract where liquidity will be added.
+     * - `token_a`: The address or identifier of the first token to be contributed.
+     * - `token_b`: The address or identifier of the second token to be contributed.
+     * - `amount_a_desired`: The desired amount of `token_a` to be contributed by the user.
+     * - `amount_b_desired`: The desired amount of `token_b` to be contributed by the user.
+     * - `amount_a_min`: The minimum amount of `token_a` acceptable for the contribution.
+     * - `amount_b_min`: The minimum amount of `token_b` acceptable for the contribution.
+     * - `address_to`: The recipient's address for receiving LP tokens.
+     * - `deadline`: The deadline by which the liquidity addition must occur.
+     *
+     * This function allows users to provide liquidity to a pool by specifying the tokens they want to
+     * contribute, the desired amounts, and minimum acceptable amounts. It also specifies the recipient's
+     * address for receiving LP (Liquidity Provider) tokens and a deadline for the operation.
+     */
+    AddLiquidity(AddLiquidityParams),
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+#[serde(rename_all = "snake_case")]
+pub struct AddLiquidityParams {
+    pub pool_address: String,
+    pub token_a: String,
+    pub token_b: String,
+    pub amount_a_desired: Uint256,
+    pub amount_b_desired: Uint256,
+    pub amount_a_min: Uint256,
+    pub amount_b_min: Uint256,
+    pub address_to: String,
+    pub deadline: Uint256,
+}
 
 /// Message type for `migrate` entry_point
 #[cw_serde]
